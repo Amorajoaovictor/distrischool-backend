0593dfe72fbcf85a35733dad36ef836c02510c91
amorajoaovictor <amorajoaovictor2@gmail.com>
2025-11-21 05:19:18 -0300

    feat(kafka): implementa CourseEventProducer para publicacao de eventos
    - Publica eventos em 4 topics: course-events, disciplina-events, matricula-events, avaliacao-events
    
    - Adiciona @EnableKafka no CourseServiceApplication
    
    - Integra producer no CursoService para publicar CREATED events

--- course-service/src/main/java/br/unifor/distrischool/course_service/CourseServiceApplication.java
+++ course-service/src/main/java/br/unifor/distrischool/course_service/CourseServiceApplication.java
@@ -2,8 +2,10 @@ package br.unifor.distrischool.course_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class CourseServiceApplication {
 
 	public static void main(String[] args) {
